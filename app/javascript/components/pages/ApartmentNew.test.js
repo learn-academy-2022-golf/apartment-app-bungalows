971b16430a5c97ad272500164c893259c996df53
Charlean Baxter <baxtercm@outlook.com>
2023-02-09 22:54:51 -0800

    testing the ApartmentNew.js using fireEvents and userEvents

--- app/javascript/components/pages/ApartmentNew.test.js
+++ app/javascript/components/pages/ApartmentNew.test.js
@@ -1,10 +1,60 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
 import ApartmentNew from "./ApartmentNew"
+import userEvent from "@testing-library/user-event"
 
 describe("<ApartmentNew />", () => {
+  beforeEach(() => {
+    const current_user = {
+      email: "test@example.com",
+      password: "password",
+      id: 1
+    }
+
+    // mock function to represent createApartment()
+    const mock = jest.fn()
+
+    render(
+      <BrowserRouter>
+        <ApartmentNew currentUser={current_user} createApt={mock}/>
+      </BrowserRouter>
+    )
+    // screen.debug()
+  })
+
   it("renders without crashing", () => {
-    const div = document.createElement("div")
-    render(<ApartmentNew />, div)
+    // screen.debug()
+    // screen.logTestingPlaygroundURL()
+    expect(screen.getByRole("button", {
+      name: /submit apartment/i
+    })).toBeInTheDocument()
+  })
+
+  it("redirects to MyApartment", () => {
+    const submitButton = screen.getByRole("button", {
+      name: /submit apartment/i
+    })
+    // screen.debug()
+    fireEvent.click(submitButton)
+    expect(location.pathname).toBe("/apartmentindex")
+  })
+
+  // userEvent
+  test("render street input", () => {
+    const newInput = screen.getAllByRole("textbox")
+    expect(newInput[0]).toBeInTheDocument()
+    expect(newInput[0]).toHaveAttribute("name", "street")
+    userEvent.type(newInput[0], "227 Queens Ct")
+    expect(newInput[0]).toHaveValue("227 Queens Ct")
+  })
+
+  // fireEvent
+  test("should change input value", () => {
+    const cityInputElement = screen.getByPlaceholderText(/enter city/i)
+    fireEvent.change(cityInputElement, {
+      target: { value: "Fake City" }
+    })
+    expect(cityInputElement.value).toBe("Fake City")
   })
 })
